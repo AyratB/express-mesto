6a9dc09fc7ce2bd3932d7184686a29b4908c004f
Ayrat Burnashev <airatbyr@yandex.ru>
2021-08-03 21:21:23 +0300

    feat: users controllers

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-// Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb ', {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-    useFindAndModify: false
+  useFindAndModify: false
 });
 
+app.use('/users', require('./routes/users'));
+
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`Запускаемся на порту №${PORT}`);
 });
 
