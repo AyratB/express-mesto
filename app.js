599f88840b8235e77e94d5c1c13e37d5e7a4c37a
Ayrat Burnashev <airatbyr@yandex.ru>
2021-08-04 08:56:42 +0300

    feat: + cards controllers

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '610a28c8bef8c25454ca35e5'
+  };
+
+  next();
+});
+
 app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.listen(PORT, () => {
   console.log(`Запускаемся на порту №${PORT}`);
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,6 @@
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
-  console.log("GET" + req);
-
   User.find({})
     .then(users => res.send({ data: users }))
     .catch(err => res.status(500).send({ message: err.message }));
